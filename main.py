d095d7684a5be6774fb1ab2c36904882e26cb614
raverken <kenny.verbeke@gmail.com>
2025-05-05 21:12:30 +0200

    ch2l2

--- main.py
+++ main.py
@@ -2,10 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import constants
+
+
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
-
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
